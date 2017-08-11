cd6c8b7bb36dbb612fa9889fcf86af9a3ffd4f29
bholman <dolorosul@gmail.com>
2017-08-11 07:54:00 -0500

    spawn controller might work

--- src/processes/SpawnController.ts
+++ src/processes/SpawnController.ts
@@ -2,13 +2,14 @@ import { BasicProcess } from "../kernel/processes/BasicProcess";
 
 export interface SpawnControllerMemory {
   spawnQueue: Array<{ name: string; request: SpawnRequest }>;
+  spawnStatus: { [name: string]: EPosisSpawnStatus };
 }
 
 export interface SpawnRequest {
   rooms: string[];
-  body: string[][];
+  body: BodyPartType[][];
   priority?: number | undefined;
-  pid?: string | number | undefined;
+  pid?: PosisPID | undefined;
 }
 
 export class SpawnController extends BasicProcess<SpawnControllerMemory> implements IPosisSpawnExtension {
@@ -18,10 +19,54 @@ export class SpawnController extends BasicProcess<SpawnControllerMemory> impleme
   }
 
   public run(): void {
+    for (const name in Memory.creeps) {
+      if (!Game.creeps[name]) {
+        delete Memory.creeps[name];
+        delete this.memory.spawnStatus[name];
+      }
+    }
+
     if (this.memory.spawnQueue.length === 0) {
       this.sleep(100);
       return;
     }
+    const spawns = _.filter(Game.spawns, (spawn: Spawn) => !spawn.spawning);
+
+    for (let i = this.memory.spawnQueue.length - 1; i >= 0; i--) {
+      const req = this.memory.spawnQueue[i];
+      const rankedSpawns = this.rankSpawns(spawns, req.request);
+      const spawnCost = this.calculateBodyCost(req.request.body[0]);
+
+      for (let j = 0; j < rankedSpawns.length; j++) {
+        if (rankedSpawns[j].rank - spawnCost >= 0) {
+          const rslt = rankedSpawns[j].spawn.createCreep(req.request.body[0], req.name);
+          if (_.isString(rslt)) {
+            spawns.splice(rankedSpawns[j].index, 1);
+            this.memory.spawnQueue.splice(i, 1);
+            this.memory.spawnStatus[req.name] = EPosisSpawnStatus.SPAWNING;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  private rankSpawns(spawns: StructureSpawn[], request: SpawnRequest):
+    Array<{ index: number, rank: number, spawn: StructureSpawn }> {
+
+    return _.map(spawns, (spawn: Spawn, index: number) => {
+      const dist = Game.map.getRoomLinearDistance(spawn.room.name, request.rooms[0]);
+      const rank = spawn.room.energyAvailable - (dist * 50);
+      return { index, rank, spawn };
+    }).sort((s) => s.rank);
+  }
+
+  private calculateBodyCost(body: BodyPartType[]): number {
+    let cost = 0;
+    for (let i = 0; i < body.length; i++) {
+      cost += BODYPART_COST[body[i] as string];
+    }
+    return cost;
   }
 
   private generateName(): string {
@@ -33,17 +78,29 @@ export class SpawnController extends BasicProcess<SpawnControllerMemory> impleme
   }
 
   public spawnCreep(opts: SpawnRequest): string {
+    if (opts.body.length === 0) { throw new Error("no body def given"); }
     const req = { name: this.generateName(), request: opts };
     this.memory.spawnQueue.push(req);
+    this.memory.spawnStatus[req.name] = EPosisSpawnStatus.QUEUED;
     return req.name;
   }
 
   public getStatus(id: string): { status: EPosisSpawnStatus; message?: string | undefined; } {
-    throw new Error("Method not implemented.");
+    const stat = { status: this.memory.spawnStatus[id] || EPosisSpawnStatus.ERROR };
+    if (stat.status === EPosisSpawnStatus.SPAWNING && Game.creeps[id] && !Game.creeps[id].spawning) {
+      this.memory.spawnStatus[id] = EPosisSpawnStatus.SPAWNED;
+      stat.status = EPosisSpawnStatus.SPAWNED;
+    }
+    return stat;
   }
 
   public getCreep(id: string): Creep | undefined {
-    throw new Error("Method not implemented.");
+    const stat = this.getStatus(id);
+    if (stat.status === EPosisSpawnStatus.SPAWNED) {
+      return Game.creeps[id];
+    } else {
+      return undefined;
+    }
   }
 
 }
