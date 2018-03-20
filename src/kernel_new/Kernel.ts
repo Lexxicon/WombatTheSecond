cc9db7ff5d4c875420c0c6a386d8a69b2dc7a1d2
Brian Holman <bholman@lexxiconstudios.com>
2018-03-20 13:59:57 -0500

    testing new kernel typings

--- src/kernel_new/Kernel.ts
+++ src/kernel_new/Kernel.ts
@@ -3,26 +3,56 @@ export type PID = string | number;
 interface ProcessImage<InitialMemory, MemType extends InitialMemory> {
   imageName: string;
   getDefaultMemory(): MemType;
-  new(context: ProcessContext<MemType>): WombatProcess;
+  new(context: ProcessContext<MemType>): Process;
 }
 
 interface ProcessContext<InitialMemory> {
   initMem: InitialMemory;
 }
 
-export interface Kernal {
+interface Process {
+  id: PID;
+}
+
+interface TestIniMem {
+  one: number;
+}
+
+interface TestMem {
+  one: number;
+  two: number;
+}
+
+class TestImage implements Process {
+  public static imageName = "TestImageName";
+
+  public static getDefaultMemory(): TestMem {
+    throw new Error("Method not implemented.");
+  }
+
+  public id: PID;
+
+  constructor(context: ProcessContext<TestIniMem>) {
+    this.id = "";
+  }
+}
+
+const t: ProcessImage<TestIniMem, TestMem> = TestImage;
+
+export interface Kernel {
   startProcess<T, M extends T>(image: ProcessImage<T, M>, initMem: T): PID;
   killProcess(pid: PID): void;
   setParent(pid: PID, parentID?: PID): boolean;
 }
 
+// tslint:disable-next-line:max-classes-per-file
 export class WombatKernel {
   public testInvoke(): void {
-    const pImg = {
-      imageName: "te",
-      getDefaultMemory() { return { one: 1, two: 2 }; },
-      new(context: ProcessContext<{ one: number, two: number }>) { return { run() { /**/ }, notify() { /**/ } }; }
+    const iMem = {
+      one: 1
     };
+
+    const rs = this.startProcess(TestImage, iMem);
   }
 
   public startProcess<T, M extends T>(image: ProcessImage<T, M>, initMem: T): PID {
